1e7e747a1942ca1252aa82d9d1690ef3ac2a0ac7
trong04092004 <hoangminhtrong2004hmt2@gmail.com>
2026-05-12 18:20:12 +0700

    xóa service

--- comment-service/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ratings/', views.RatingListCreate.as_view(), name='ratings'),
-    path('ratings/list/', views.RatingList.as_view(), name='ratings-list'),
-    path('health/security/', views.security_health, name='security-health'),
-]
--- manager-service/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('promotions/', views.PromotionListCreate.as_view(), name='promotions'),
-    path('supply-orders/', views.SupplyOrderListCreate.as_view(), name='supply-orders'),
-    path('health/security/', views.security_health, name='security-health'),
-]
